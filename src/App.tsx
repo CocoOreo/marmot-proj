5ba1e3b31d0e5cf4236ebb9f3acf796827bd6b3f
CocoOreo <songhaoranuestc@163.com>
2021-09-19 14:45:37 +0800

    chore: Initialize React App and add envdot and emotion

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log("当前环境为：" + process.env.NODE_ENV)
+  console.log("连接的API为：" + process.env.REACT_APP_BASE_URL)
   return (
     <div className="App">
       <header className="App-header">
