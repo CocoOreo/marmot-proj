64a0e92625471a98f3d861df09abf92d8a8c8fa1
CocoOreo <songhaoranuestc@163.com>
2021-09-30 22:39:14 +0800

    feat(error-boundary): Add the ErrorBoundary.

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,18 +1,29 @@
 import styled from "@emotion/styled";
+import { Alert } from 'antd';
+import React from "react";
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => {
+    return (
+        <div>
+            <Alert message={error} type="error" />
+        </div>
+    )
+}
 
 export const Row = styled.div<{
-    gap?:number | boolean;
+    gap?: number | boolean;
     between?: boolean;
-    marginBottom?:number;
+    marginBottom?: number;
 }>`
     display: flex;
     align-items: center;
     justify-content: ${(props) => (props.between ? "space-between" : undefined)};
-    margin-bottom: ${(props) => props.marginBottom? props.marginBottom + "rem" : 0};
+    margin-bottom: ${(props) => props.marginBottom ? props.marginBottom + "rem" : 0};
     > * {
         /* To avoid the margin top of items affect the align-items;center */
         margin-top: 0 !important;
         margin-bottom: 0 !important;
-        margin-right: ${props => typeof props.gap === 'number' ? props.gap + 'rem':props.gap? '2rem' : undefined};
+        margin-right: ${props => typeof props.gap === 'number' ? props.gap + 'rem' : props.gap ? '2rem' : undefined};
     }
 `
+
